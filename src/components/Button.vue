dec31f51543e55bdffba2885f55cef7c7004cf7d
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-11-27 00:14:21 +1300

    Vue CLI Setup -> Dynamic Classes

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template lang="">
+     <div>
+          <button @click="onClick()" :style="{background: color}" class="btn">{{ text }}</button>
+     </div>
+</template>
+
+<script>
+export default {
+     name: 'Button',
+     props: {
+          text: {
+               type: String,
+               default: 'Add Task',
+          },
+
+          color: {
+               type: String,
+               default: 'green',
+          },
+     },
+     methods: {
+          onClick() {
+               console.log('click');
+          }
+     }
+}
+</script>
+
+<style scoped>
+     
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template lang="">
+     <div v-for="task in tasks" :key="task.id">
+          <Task :task="task" />
+     </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+     name: 'Tasks',
+     props: {
+          tasks: Array
+     },
+     components: {
+          Task,
+     },
+}
+</script>
+
+<style lang="">
+     
+</style>
